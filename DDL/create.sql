ef1cf6a088fca674ef4f26c49963b72ca4c0816b
mainka18 <131583614+mainka18@users.noreply.github.com>
2023-08-17 21:33:31 +0530

    Added CREATE TABLE query for Employee

--- DDL/create.sql
+++ DDL/create.sql
@@ -1,2 +1,14 @@
 --  CREATE statements for tables
-CREATE TABLE ...
+-- DDL/create.sql
+
+-- Create Employee table
+CREATE TABLE Employee (
+    EmployeeID INT PRIMARY KEY,
+    MID INT, -- Manager ID
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100) UNIQUE,
+    Department VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    FOREIGN KEY (MID) REFERENCES Employee(EmployeeID)
+);
