28c5078280a6d3d366edede9c324a726296e1275
mainka18 <131583614+mainka18@users.noreply.github.com>
2023-08-17 22:53:07 +0530

    Update query7.sql

--- DML/query7.sql
+++ DML/query7.sql
@@ -1,7 +1,7 @@
 -- DML/query7.sql
 
--- Find employees of class A with manager in class B
+-- Find employees of class A with manager in class B using self-join
 SELECT e.EmployeeID, e.FirstName, e.LastName, e.Class, m.FirstName AS ManagerFirstName, m.LastName AS ManagerLastName
 FROM Employee e
-LEFT JOIN Employee m ON e.MID = m.EmployeeID AND m.Class = 'Class B'
-WHERE e.Class = 'Class A' AND m.EmployeeID IS NOT NULL;
+JOIN Employee m ON e.MID = m.EmployeeID
+WHERE e.Class = 'Class A' AND m.Class = 'Class B';
