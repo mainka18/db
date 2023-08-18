b2b21716bfad6adbeb5b387d99fe37dac3785e0a
mainka18 <131583614+mainka18@users.noreply.github.com>
2023-08-18 13:24:25 +0530

    Update query5.sql

--- DML/query5.sql
+++ DML/query5.sql
@@ -1,6 +1,15 @@
 -- DML/query5.sql
 
 -- Find maximum salary in each class
-SELECT Class, MAX(Salary) AS MaxSalary
-FROM Employee
-GROUP BY Class;
+--SELECT Class, MAX(Salary) AS MaxSalary
+--FROM Employee
+--GROUP BY Class;
+--changes with employee info 
+-- Find employee(s) with maximum salary in each class
+SELECT e.EmployeeID, e.FirstName, e.LastName, e.Class, e.Salary
+FROM Employee e
+WHERE e.Salary = (
+    SELECT MAX(Salary)
+    FROM Employee
+    WHERE Class = e.Class
+);
