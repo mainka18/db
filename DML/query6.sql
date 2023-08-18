02cfd200400f22754d775119bd7d41bd9e149713
mainka18 <131583614+mainka18@users.noreply.github.com>
2023-08-18 13:27:07 +0530

    Update query6.sql

--- DML/query6.sql
+++ DML/query6.sql
@@ -1,6 +1,15 @@
 -- DML/query6.sql
 
 -- Find minimum salary in each class
-SELECT Class, MIN(Salary) AS MinSalary
-FROM Employee
-GROUP BY Class;
+--SELECT Class, MIN(Salary) AS MinSalary
+--FROM Employee
+--GROUP BY Class;
+-- Find employee(s) with minimum salary in each class
+--changes employee info 
+SELECT e.EmployeeID, e.FirstName, e.LastName, e.Class, e.Salary
+FROM Employee e
+WHERE e.Salary = (
+    SELECT MIN(Salary)
+    FROM Employee
+    WHERE Class = e.Class
+);
